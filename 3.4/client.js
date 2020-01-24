b7fbb8b40e309c5b87ceec84841403953ffe4c34
VukasinVujic <vukasinvujic021@gmail.com>
2020-01-24 12:52:18 +0100

    making a small application for adding, deleting and listing

--- 3.4/client.js
+++ 3.4/client.js
@@ -1,22 +1,47 @@
+// const EventEmitter = require('events');
+// const readline = require('readline');
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
+
+// const client = new EventEmitter();
+// const server = require('./server')(client);
+
+// server.on('response', (resp) => {
+//   process.stdout.write('\u001B[2J\u001B[0;0f');
+//   process.stdout.write(resp);
+//   process.stdout.write('\n\> ');
+// });
+
+// let command, args;
+// rl.on('line', (input) => {
+//   [command, ...args] = input.split(' ');
+//   client.emit('command', command, args);
+// });
+
+
 const EventEmitter = require('events');
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+    input: process.stdin,
+    output: process.stdout
+})
 
 const client = new EventEmitter();
 const server = require('./server')(client);
 
 server.on('response', (resp) => {
-  process.stdout.write('\u001B[2J\u001B[0;0f');
-  process.stdout.write(resp);
-  process.stdout.write('\n\> ');
-});
+    process.stdout.write('\u001B[2J\u001B[0;0f');
+    process.stdout.write(resp);
+    process.stdout.write('\n\> ');
+})
 
-let command, args;
+let command,args;
+
 rl.on('line', (input) => {
-  [command, ...args] = input.split(' ');
-  client.emit('command', command, args);
-});
+  [command, ...args] = input.split(' '); 
+  client.emit('command', command, args );
+})
--- 3.4/server.js
+++ 3.4/server.js
@@ -1,55 +1,111 @@
+// const EventEmitter = require('events');
+
+// class Server extends EventEmitter {
+//   constructor(client) {
+//     super();
+//     this.tasks = {};
+//     this.taskId = 1;
+//     process.nextTick(() => {
+//       this.emit(
+//         'response',
+//         'Type a command (help to list commands)'
+//       );
+//     });
+//     client.on('command', (command, args) => {
+//       switch (command) {
+//       case 'help':
+//       case 'add':
+//       case 'ls':
+//       case 'delete':
+//         this[command](args);
+//         break;
+//       default:
+//         this.emit('response', 'Unknown command...');
+//       }
+//     });
+//   }
+
+//   tasksString() {
+//     return Object.keys(this.tasks).map(key => {
+//       return `${key}: ${this.tasks[key]}`;
+//     }).join('\n');
+//   }
+
+//   help() {
+//     this.emit('response', `Available Commands:
+//   add task
+//   ls
+//   delete :id`
+//     );
+//   }
+//   add(args) {
+//     this.tasks[this.taskId] = args.join(' ');
+//     this.emit('response', `Added task ${this.taskId}`);
+//     this.taskId++;
+//   }
+//   ls() {
+//     this.emit('response', `Tasks:\n${this.tasksString()}`);
+//   }
+//   delete(args) {
+//     delete(this.tasks[args[0]]);
+//     this.emit('response', `Deleted task ${args[0]}`);
+//   }
+// }
+
+// module.exports = (client) => new Server(client);
+
+
 const EventEmitter = require('events');
 
 class Server extends EventEmitter {
-  constructor(client) {
-    super();
-    this.tasks = {};
-    this.taskId = 1;
-    process.nextTick(() => {
-      this.emit(
-        'response',
-        'Type a command (help to list commands)'
-      );
-    });
-    client.on('command', (command, args) => {
-      switch (command) {
-      case 'help':
-      case 'add':
-      case 'ls':
-      case 'delete':
-        this[command](args);
+   constructor(client) {
+     super();
+     this.tasks = {}; // to hold task information
+     this.taskId = 1; // to have an id 
+     process.nextTick(() => { // so the response event  would run before everyting
+       this.emit('response' ,'Type comand (help to list commands');
+     })
+     client.on('command' , (command, args) => {
+      //  console.log(`Comand: ${command}`);
+      //  help, add , ls , delete
+      switch(command){
+        case 'help':
+        case 'add':
+        case 'ls':
+        case 'delete':
+          this[command](args); // to choose on of the commands above, args is what user types
         break;
-      default:
+        default:  
         this.emit('response', 'Unknown command...');
+        
       }
-    });
-  }
+     })
+   }
+   tasksString(){
+     return Object.keys(this.tasks).map(key => {
+        return `${key}: ${this.tasks[key]}`;
+     }).join('\n');
+   }
 
-  tasksString() {
-    return Object.keys(this.tasks).map(key => {
-      return `${key}: ${this.tasks[key]}`;
-    }).join('\n');
-  }
-
-  help() {
-    this.emit('response', `Available Commands:
-  add task
-  ls
-  delete :id`
-    );
-  }
-  add(args) {
+   help(){
+     this.emit('response', `Availabe commands are:
+      add task
+      ls
+      delete: id`);
+   }
+   add(args){
     this.tasks[this.taskId] = args.join(' ');
     this.emit('response', `Added task ${this.taskId}`);
     this.taskId++;
   }
-  ls() {
-    this.emit('response', `Tasks:\n${this.tasksString()}`);
+  ls(){
+    this.emit('response', `Tasks:\n${this.tasksString()}`) 
   }
-  delete(args) {
+  delete(args){
     delete(this.tasks[args[0]]);
-    this.emit('response', `Deleted task ${args[0]}`);
+    this.emit('response', `Deleted task ${args[0]}`)
   }
+    
 }
 
-module.exports = (client) => new Server(client);
+module.exports =(client) => new Server(client);
