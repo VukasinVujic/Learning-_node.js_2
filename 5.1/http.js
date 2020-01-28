6f6336f534303b39798d4a45d8a704979535f77e
VukasinVujic <vukasinvujic021@gmail.com>
2020-01-28 17:06:02 +0100

    learning about http

--- 5.1/http.js
+++ 5.1/http.js
@@ -1,16 +1,35 @@
+// const server = require('http').createServer();
+
+// server.on('request', (req, res) => {
+//   res.writeHead(200, { 'content-type': 'text/plain' });
+//   res.write('Hello world\n');
+
+//   setTimeout(function () {
+//     res.write('Another Hello world\n');
+//   }, 10000);
+
+//   setTimeout(function () {
+//     res.write('Yet Another Hello world\n');
+//   }, 20000);
+// });
+
+// server.listen(8000);
+
+
 const server = require('http').createServer();
 
-server.on('request', (req, res) => {
-  res.writeHead(200, { 'content-type': 'text/plain' });
+server.on('request', (req,res) => {
+  res.writeHead(200, { 'content-type': 'text/plain'  }); 
   res.write('Hello world\n');
 
-  setTimeout(function () {
-    res.write('Another Hello world\n');
-  }, 10000);
+  setTimeout(function() {
+    res.write('lallalaal\n');
+  }, 1000);
 
-  setTimeout(function () {
-    res.write('Yet Another Hello world\n');
-  }, 20000);
-});
+  setTimeout(function() {
+    res.write('crno beli svet\n');
+  }, 2000);
+
+})
 
 server.listen(8000);
