d2d66d581404971f2419970e072476eb18066b2f
VukasinVujic <vukasinvujic021@gmail.com>
2020-01-28 18:48:54 +0100

    routing

--- 5.4/server.js
+++ 5.4/server.js
@@ -1,26 +1,58 @@
-const fs = require('fs');
+// const fs = require('fs');
+// const server = require('http').createServer();
+// const data = {};
+
+// server.on('request', (req, res) => {
+//   switch (req.url) {
+//   case '/api':
+//     res.writeHead(200, { 'Content-Type': 'application/json' });
+//     res.end(JSON.stringify(data));
+//     break;
+//   case '/home':
+//   case '/about':
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end(fs.readFileSync(`.${req.url}.html`));
+//     break;
+//   case '/':
+//     res.writeHead(301, { 'Location': '/home' });
+//     res.end();
+//     break;
+//   default:
+//     res.writeHead(404);
+//     res.end();
+//   }
+// });
+
+// server.listen(8000);
+
+
 const server = require('http').createServer();
+const fs = require('fs');
+
 const data = {};
 
-server.on('request', (req, res) => {
-  switch (req.url) {
-  case '/api':
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(data));
+server.on('request', (req,res) => {
+  
+  switch(req.url){
+    case '/api':
+      res.writeHead(200, {'Content-type':'application/json'}) 
+      res.end(JSON.stringify(data))
+     
     break;
-  case '/home':
-  case '/about':
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end(fs.readFileSync(`.${req.url}.html`));
-    break;
-  case '/':
-    res.writeHead(301, { 'Location': '/home' });
-    res.end();
-    break;
-  default:
-    res.writeHead(404);
-    res.end();
+    case '/home':
+    case '/about':  
+      res.writeHead(200, {'Content-type':'text/html'});
+      res.end(fs.readFileSync(`./${req.url}.html`))
+      res.end('Hello world\n');
+    case '/':  
+      res.writeHead(301, {'Location':'/home'})
+      res.end();
+      break;
+      default: 
+      res.writeHead(404);
+      res.end();
   }
-});
+  
+})
 
 server.listen(8000);
