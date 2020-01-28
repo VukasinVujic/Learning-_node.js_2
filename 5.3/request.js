20b4496765f2bfdf108c2f8a4f763f7603e23b7d
VukasinVujic <vukasinvujic021@gmail.com>
2020-01-28 17:56:21 +0100

    request HTTP

--- 5.3/request.js
+++ 5.3/request.js
@@ -1,19 +1,39 @@
-const https = require('https');
+// const https = require('https');
 
-// req: http.ClientRequest
-const req = https.get(
-  'https://www.google.com',
+// // req: http.ClientRequest
+// const req = https.get(
+//   'https://www.google.com',
+//   (res) => {
+//     // res: http.IncomingMessage
+//     console.log(res.statusCode);
+//     console.log(res.headers);
+
+//     res.on('data', (data) => {
+//       console.log(data.toString());
+//     });
+//   }
+// );
+
+// req.on('error', (e) => console.log(e));
+
+// console.log(req.agent); // http.Agent
+
+const http = require('http');
+
+// const req = http.request( {hostname: 'www.google.com'
+
+const req = http.get(
+  'http://www.google.com',
   (res) => {
-    // res: http.IncomingMessage
     console.log(res.statusCode);
     console.log(res.headers);
-
+    
     res.on('data', (data) => {
       console.log(data.toString());
-    });
+    })
   }
 );
 
 req.on('error', (e) => console.log(e));
 
-console.log(req.agent); // http.Agent
+console.log(req.agent);
